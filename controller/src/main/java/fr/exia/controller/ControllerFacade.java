ac03cee7d9102ad0fbdcccd71e13e5815bc62a39
araubase <33415682+araubase@users.noreply.github.com>
2018-05-31 09:35:31 +0200

    Beginning of development

---
+++ controller/src/main/java/fr/exia/controller/ControllerFacade.java
@@ -0,0 +1,72 @@
+package fr.exia.controller;
+
+import java.sql.SQLException;
+
+/**
+ * <h1>The Class ControllerFacade provides a facade of the Controller component.</h1>
+ *
+ * @author Yoan BIQUET yoan.biquet@viacesi.fr 
+ * @version 2.0
+ */
+
+public class ControllerFacade implements IController {
+
+    /** The view. */
+    private final IView  view;
+
+    /** The model. */
+    private final IModel model;
+
+    /**
+     * Instantiates a new controller facade.
+     *
+     * @param view
+     *            the view
+     * @param model
+     *            the model
+     */
+    public ControllerFacade(final IView view, final IModel model) {
+        super();
+        this.view = view;
+        this.model = model;
+    }
+    
+    /**
+     * Start.
+     *
+     * @throws SQLException
+     *             the SQL exception
+     */
+    public void start() throws SQLException {
+        this.getView().displayMessage(this.getModel().getExampleById(1).toString());
+
+        this.getView().displayMessage(this.getModel().getExampleByName("Example 2").toString());
+
+        final List<Example> examples = this.getModel().getAllExamples();
+        final StringBuilder message = new StringBuilder();
+        for (final Example example : examples) {
+            message.append(example);
+            message.append('\n');
+        }
+        this.getView().displayMessage(message.toString());
+    }
+    
+    /**
+     * Gets the view.
+     *
+     * @return the view
+     */
+    public IView getView() {
+        return this.view;
+    }
+
+    /**
+     * Gets the model.
+     *
+     * @return the model
+     */
+    public IModel getModel() {
+        return this.model;
+    }
+}
+
